9feb3e83296cddd75e18c8c3d192efc4f210ef37
Nandaram Prashanth kumar <kmrprashanth92@gmail.com>
2023-12-06 18:10:19 -0500

    add to  cart and price added

--- screens/AfterLogin.js
+++ screens/AfterLogin.js
@@ -1,14 +1,14 @@
 // AfterLogin.js
 import React, { useState } from 'react';
 import { View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'; // Import the heart icon
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import the heart and cart icons
 
 const AfterLogin = () => {
   const [artItems, setArtItems] = useState([
-    { id: '1', image: require('../assets/image1.webp'), isFavorite: false },
-    { id: '2', image: require('../assets/image2.jpg'), isFavorite: false },
-    { id: '3', image: require('../assets/image3.avif'), isFavorite: false },
-    { id: '4', image: require('../assets/image4.jpeg'), isFavorite: false },
+    { id: '1', image: require('../assets/image1.webp'), isFavorite: false, price: 29.99 },
+    { id: '2', image: require('../assets/image2.jpg'), isFavorite: false, price: 39.99 },
+    { id: '3', image: require('../assets/image3.avif'), isFavorite: false, price: 49.99 },
+    { id: '4', image: require('../assets/image4.jpeg'), isFavorite: false, price: 59.99 },
   ]);
 
   const toggleFavorite = (itemId) => {
@@ -19,9 +19,20 @@ const AfterLogin = () => {
     );
   };
 
+  const addToCart = (itemId) => {
+    // Add logic to handle adding item to the cart
+    console.log(`Item ${itemId} added to cart`);
+  };
+
   const renderArtItem = ({ item }) => (
     <View style={styles.artItemContainer}>
       <Image source={item.image} style={styles.artImage} />
+      <View style={styles.priceContainer}>
+        <Text style={styles.price}>${item.price.toFixed(2)}</Text>
+        <TouchableOpacity onPress={() => addToCart(item.id)} style={styles.cartButton}>
+          <Icon name="cart-plus" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity onPress={() => toggleFavorite(item.id)} style={styles.favoriteButton}>
         <Icon name={item.isFavorite ? 'heart' : 'heart-o'} size={24} color="#FF0000" />
       </TouchableOpacity>
@@ -54,6 +65,7 @@ const styles = StyleSheet.create({
   artItemContainer: {
     flex: 1,
     margin: 8,
+    position: 'relative',
   },
   artImage: {
     width: '100%',
@@ -61,10 +73,27 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 8,
   },
+  priceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cartButton: {
+    backgroundColor: '#000000',
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,  // Adjusted padding for a smaller size
+  },
   favoriteButton: {
     position: 'absolute',
-    top: 10,
-    right: 10,
+    top: 8,
+    right: 8,
   },
 });
 
