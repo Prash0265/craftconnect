66f2c599718bcc295e5a3a33de56265348970c38
Prash0265 <kmrprashanth92@gmail.com>
2023-12-06 01:21:17 -0500

    added splash screen

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
-//App.js
-import React from 'react';
+// App.js
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+ 
+import SplashScreen from './screens/SplashScreen';  // Import SplashScreen component
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
-import BottomTabNavigator from './screens/BottomTabNavigator'; 
-
+import BottomTabNavigator from './screens/BottomTabNavigator';
+ 
 const Stack = createStackNavigator();
+ 
 export default function App() {
+  const [isSplashVisible, setSplashVisible] = useState(true);
+ 
+  useEffect(() => {
+    // Simulate a delay for the splash screen
+    const splashTimer = setTimeout(() => {
+      setSplashVisible(false);
+    }, 3000);  // Adjust the duration as needed
+ 
+    return () => clearTimeout(splashTimer);
+  }, []);
+ 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
-     
-      </Stack.Navigator>
-    </NavigationContainer>
+<NavigationContainer>
+<Stack.Navigator initialRouteName="Splash">
+        {/* Add the SplashScreen screen */}
+        {isSplashVisible ? (
+<Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        ) : (
+<>
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Login" component={LoginScreen} />
+<Stack.Screen name="Signup" component={SignupScreen} />
+<Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
+</>
+        )}
+</Stack.Navigator>
+</NavigationContainer>
   );
 }
+
