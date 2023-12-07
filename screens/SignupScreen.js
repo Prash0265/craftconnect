74f0bb916bf11c0f73a8b1b59fa38ec8e893707f
Rajanmeet Kaur <115046368+MeetWaraich@users.noreply.github.com>
2023-12-07 17:59:30 -0500

    Validation and Date picker

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,18 +1,63 @@
-//SignupScreen.js
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Image, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import axios from 'axios';
 
 const SignupScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [dob, setDob] = useState(''); 
+  const [dob, setDob] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [maximumDate, setMaximumDate] = useState(new Date());
+
+  useEffect(() => {
+    setMaximumDate(new Date());
+  }, []);
+
+  const validateUsername = (inputUsername) => {
+    return inputUsername.length >= 8;
+  };
+
+  const validateEmail = (inputEmail) => {
+    const emailRegex = /^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(inputEmail);
+  };
+
+  const validateDOB = (inputDOB) => {
+    // Check if inputDOB is a valid Date object
+    if (inputDOB instanceof Date && !isNaN(inputDOB)) {
+      // Add your custom validation for Date of Birth (e.g., YYYY-MM-DD format)
+      // For simplicity, let's assume any non-empty string is valid for demonstration
+      return true;
+    }
+  
+    // Return false if inputDOB is not a valid Date object
+    return false;
+  };
+  
+  
 
   const handleSignup = async () => {
     try {
-      //await axios.post('http://10.0.0.221:3000/signup', { username, password });//Baymills
-      await axios.post('http://10.51.223.182:3000/signup', {username, password ,email, dob }); //Cestar
+      if (!validateUsername(username)) {
+        Alert.alert('Invalid Username', 'Username should be at least 8 characters long.');
+        return;
+      }
+
+      if (!validateEmail(email)) {
+        Alert.alert('Invalid Email', 'Please enter a valid email address.');
+        return;
+      }
+
+      if (!validateDOB(dob)) {
+        Alert.alert('Invalid Date of Birth', 'Please enter a valid Date of Birth.');
+        return;
+      }
+
+      const formattedDOB = dob.toISOString().split('T')[0];
+
+      await axios.post('http://10.51.223.253:3000/signup', { username, password, email, dob: formattedDOB });
 
       Alert.alert('Signup Successful', 'You have successfully signed up!', [
         {
@@ -27,10 +72,21 @@ const SignupScreen = ({ navigation }) => {
     }
   };
 
+  const showDatepicker = () => {
+    setShowDatePicker(true);
+  };
+
+  const onDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || dob;
+    setShowDatePicker(Platform.OS === 'ios');
+    setDob(currentDate);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.formContainer}>
         <Text style={styles.text}>Signup</Text>
+
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.textInput}
@@ -40,6 +96,7 @@ const SignupScreen = ({ navigation }) => {
             onChangeText={(text) => setUsername(text)}
           />
         </View>
+
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.textInput}
@@ -50,38 +107,38 @@ const SignupScreen = ({ navigation }) => {
             secureTextEntry
           />
         </View>
-        <View style={styles.inputContainer}>
-  <TextInput
-    style={styles.textInput}
-    placeholder="Email"
-    placeholderTextColor="#999"
-    value={email}
-    onChangeText={(text) => setEmail(text)}
-  />
-</View>
 
-<View style={styles.inputContainer}>
-  <TextInput
-    style={styles.textInput}
-    placeholder="Date of Birth (YYYY-MM-DD)"
-    placeholderTextColor="#999"
-    value={dob}
-    onChangeText={(text) => setDob(text)}
-  />
-</View>
-        <Text
-          style={styles.button1}
-          onPress={handleSignup}
-        >
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            placeholderTextColor="#999"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Button onPress={showDatepicker} title="Select Date of Birth" />
+          {showDatePicker && (
+            <DateTimePicker
+              value={dob}
+              mode="date"
+              display="default"
+              onChange={onDateChange}
+              maximumDate={maximumDate}
+              style={styles.dateTimePicker}
+            />
+          )}
+        </View>
+
+        <Text style={styles.button1} onPress={handleSignup}>
           Signup
         </Text>
       </View>
     </View>
   );
 };
-
-
-export default SignupScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -92,8 +149,8 @@ const styles = StyleSheet.create({
   formContainer: {
     width: '80%',
     padding: 20,
-    backgroundColor: '#fff', // Background color for the form container
-    borderRadius: 10, // Border radius for the form container
+    backgroundColor: '#fff',
+    borderRadius: 10,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -110,9 +167,9 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     marginBottom: 20,
-    borderWidth: 1,
-    borderColor: '#262E36', // Border color for the text input container
-    borderRadius: 8, // Border radius for the text input container
+    borderBottomWidth: 1,
+    borderColor: '#262E36',
+    borderRadius: 8,
   },
   button1: {
     backgroundColor: '#262E36',
@@ -131,9 +188,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     width: '100%',
   },
-  logo: {
-    height: '20%',
-    resizeMode: 'contain',
-    marginBottom: 50,
+  dateTimePicker: {
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: '#000000',
+    borderRadius: 8,
+    marginTop: 10,
+    marginBottom: 20,
   },
 });
+
+export default SignupScreen
